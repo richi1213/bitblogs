24f070018ee3092b60f90b2653888f0185871b6e
richi1213 <datonanava11@gmail.com>
2024-11-17 18:25:33 +0400

    added footer paragraph tranlsation

--- src/components/layout/components/footer/footer.tsx
+++ src/components/layout/components/footer/footer.tsx
@@ -1,8 +1,11 @@
+import { useTranslation } from 'react-i18next';
+
 const Footer: React.FC = () => {
+  const { t } = useTranslation('footer');
   return (
     <footer className='mt-12 border-t bg-muted/50'>
       <div className='container mx-auto px-4 py-6 text-center text-muted-foreground'>
-        <p>© 2024 BitBlogs. All rights reserved.</p>
+        <p>{t('2024-arr')}</p>
       </div>
     </footer>
   );
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import navbarEn from '@/i18n/en/layout/navbar.json';
+import footerEn from '@/i18n/en/layout/footer.json';
 import homePageEn from '@/i18n/en/pages/home.json';
 import writePageEn from '@/i18n/en/pages/write.json';
 import aboutPageEn from '@/i18n/en/pages/about.json';
 import loginAndRegisterPageEn from '@/i18n/en/pages/login-and-register.json';
 
 import navbarKa from '@/i18n/ka/layout/navbar.json';
+import footerKa from '@/i18n/ka/layout/footer.json';
 import homePageKa from '@/i18n/ka/pages/home.json';
 import writePageKa from '@/i18n/ka/pages/write.json';
 import aboutPageKa from '@/i18n/ka/pages/about.json';
@@ -17,6 +19,7 @@ i18n.use(initReactI18next).init({
   resources: {
     en: {
       navbar: navbarEn,
+      footer: footerEn,
       'home-page': homePageEn,
       'write-page': writePageEn,
       'about-page': aboutPageEn,
@@ -24,6 +27,7 @@ i18n.use(initReactI18next).init({
     },
     ka: {
       navbar: navbarKa,
+      footer: footerKa,
       'home-page': homePageKa,
       'write-page': writePageKa,
       'about-page': aboutPageKa,
@@ -35,6 +39,7 @@ i18n.use(initReactI18next).init({
   defaultNS: 'navbar',
   ns: [
     'navbar',
+    'footer',
     'home-page',
     'write-page',
     'about-page',
