76866c6b5c4300e54f6ea8859f5b2e453515c9b6
richi1213 <datonanava11@gmail.com>
2024-11-21 00:22:50 +0400

    centered author page

--- src/pages/author/components/index.tsx
+++ src/pages/author/components/index.tsx
@@ -3,7 +3,7 @@ import AuthorTabs from '@/pages/author/components/author-tabs';
 
 const Author: React.FC = () => {
   return (
-    <div>
+    <div className='mx-auto max-w-4xl'>
       <AuthorDetails />
       <AuthorTabs />
     </div>
