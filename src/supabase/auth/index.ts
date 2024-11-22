de13e397538d7dc7086af94196bf9e18fa4ef814
richi1213 <datonanava11@gmail.com>
2024-11-22 23:40:12 +0400

    working on supabase errors

--- src/supabase/auth/index.ts
+++ src/supabase/auth/index.ts
@@ -1,6 +1,10 @@
 import { supabase } from '@/supabase';
 import { Database } from '@/supabase/supabase.types';
-import { SupabaseClient, AuthResponse } from '@supabase/supabase-js';
+import {
+  SupabaseClient,
+  AuthResponse,
+  isAuthApiError,
+} from '@supabase/supabase-js';
 
 const supabaseWithSchema: SupabaseClient<Database> = supabase;
 
@@ -37,23 +41,64 @@ export const register = async ({
   };
 };
 
+// export const login = async ({ email, password }: RegisterInput) => {
+//   const { data, error } = await supabaseWithSchema.auth.signInWithPassword({
+//     email,
+//     password,
+//   });
+
+//   if (error) {
+//     return {
+//       data: {
+//         user: null,
+//         session: null,
+//       },
+//       error,
+//     };
+//   }
+
+//   console.log(data);
+
+//   return {
+//     data: {
+//       user: data.user,
+//       session: data.session,
+//     },
+//     error: null,
+//   };
+// };
+
 export const login = async ({ email, password }: RegisterInput) => {
-  const { data, error } = await supabaseWithSchema.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
 
   if (error) {
+    let errorMessage = 'An error occurred. Please try again.';
+
+    if (isAuthApiError(error)) {
+      switch (error.status) {
+        case 400:
+          errorMessage = 'Invalid email or password.';
+          break;
+        case 401:
+          errorMessage = 'Unauthorized. Please check your credentials.';
+          break;
+        default:
+          errorMessage = error.message;
+          break;
+      }
+    }
+
     return {
       data: {
         user: null,
         session: null,
       },
-      error,
+      error: { ...error, message: errorMessage },
     };
   }
-
-  console.log(data);
 
   return {
     data: {
